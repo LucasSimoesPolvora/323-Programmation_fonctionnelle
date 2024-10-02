0bc7b1aa79f0d4fc329169bc84d57b946fab6d6e
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-10-02 11:45:40 +0200

    feat (silkroad): Tell if it's possible or not

--- personnel/silkroad/ConsoleApp1/Program.cs
+++ personnel/silkroad/ConsoleApp1/Program.cs
@@ -42,9 +42,9 @@ void DrawBoard(bool[,] board)
 DrawBoard(silkyWay);
 
 // TODO Create a data structure that allow us to remember which square has already been tested
-
+bool[,] testCase = new bool[8, 8];
 // TODO Create a data structure that allow us to remember the successful steps
-
+bool[,] path = new bool[8, 8];
 // TODO Write the recursive function
 // Recursive function that tells if we can reach H8 from the given position
 // The algorithm is in fact simple to spell out (even in french ;)):
@@ -57,5 +57,32 @@ DrawBoard(silkyWay);
 //          2. Je peux sortir depuis une des cases où je peux aller (et où je ne suis pas encore allé)
 
 // TODO Call the function and show the results
+bool isThereAWayOut(int x, int y)
+{
+    if(x == 7 && y == 7) return true;
+    if(x < 0 || y < 0 || x > 7 || y > 7) return false;
+    if(testCase[x, y]) return false;
+    if(!silkyWay[x, y]) return false;
+
+    testCase[x, y] = true;
+
+    if(isThereAWayOut(x - 1, y + 1) ||
+       isThereAWayOut(x - 1, y) ||
+       isThereAWayOut(x - 1, y - 1) ||
+       isThereAWayOut(x, y + 1) ||
+       isThereAWayOut(y, y - 1) ||
+       isThereAWayOut(x + 1, y - 1) ||
+       isThereAWayOut(x + 1, y) ||
+       isThereAWayOut(x + 1, y + 1))
+    {
+        path[x, y] = true;
+        return true;
+    }
+    return false;
+}
+
+bool result = isThereAWayOut(0,0);
+
+Console.WriteLine(result);
 
 Console.ReadLine();
